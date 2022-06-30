64a091aac5fd8d0022900ebd1518d92eecaad0b9
paul wanyik <sammy.k.mutua@gmail.com>
2022-06-30 12:29:44 +0300

    Creating Invokable Controllers For The Needed API Endpoints

---
+++ app/Http/Controllers/Admin/Users/ShowController.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Admin\Users;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ShowController extends Controller {
+    public function __invoke(Request $request) {
+        //
+    }
+}
