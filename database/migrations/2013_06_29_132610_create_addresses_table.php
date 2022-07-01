61cfe640e58e18d674706f5617d79580587cd20a
paul wanyik <sammy.k.mutua@gmail.com>
2022-07-01 10:54:14 +0300

    Adding On Delete Set To Null On user_id And location_id

---
+++ database/migrations/2013_06_29_132610_create_addresses_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    
+    public function up(): void {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->uuid(column: 'uuid')->unique();
+
+            $table->string(column:'label'); // House/Office/Head Office/Mums House
+            $table->boolean(column:'billing')->default(false);
+            
+             $table->foreignId('user_id')->nullable()->index()->constrained()->nullOnDelete();
+            $table->foreignId('location_id')->nullable()->index()->constrained()->nullOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void {
+        Schema::dropIfExists('addresses');
+    }
+};
